f58a785f998c3c8d3c6c95d1ad2fbde0f82c407b
Kian Ostad <c@kianostad.com>
2017-04-15 11:50:59 +0430

    update makefile

--- Makefile.mk
+++ Makefile.mk
@@ -1,31 +1,33 @@
-export SERVICE_ROOT=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+export SERVICES_ROOT=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 export GO?=$(shell which go)
 export UPDATE?=
 export GOPATH?=$(shell mktemp -d)
-export LINTER=$(GOPATH)/bin/gometalinter.v1
-export LINTERCMD?=$(LINTER) -e ".*.gen.go" -e "upstream.go" -e "tmp" --cyclo-over=19 --line-length=120 --deadline=100s --disable-all --enable=structcheck --enable=deadcode --enable=gocyclo --enable=ineffassign --enable=golint --enable=goimports --enable=errcheck --enable=varcheck --enable=goconst --enable=gosimple --enable=staticcheck --enable=misspell
+export SERVICES_LINTER=$(GOPATH)/bin/gometalinter.v1
+export SERVICES_LINTERCMD?=$(SERVICES_LINTER) -e ".*.gen.go" -e "upstream.go" -e "tmp" --cyclo-over=19 --line-length=120 --deadline=100s --disable-all --enable=structcheck --enable=deadcode --enable=gocyclo --enable=ineffassign --enable=golint --enable=goimports --enable=errcheck --enable=varcheck --enable=goconst --enable=gosimple --enable=staticcheck --enable=misspell
 
-$(SERVICE_ROOT)/tmp/ip2l/IP-COUNTRY-REGION-CITY.BIN:
-	mkdir -p $(SERVICE_ROOT)/tmp/ip2l
-	wget -c http://www.clickyab.com/downloads/IP-COUNTRY-REGION-CITY.BIN -O $(SERVICE_ROOT)/tmp/ip2l/IP-COUNTRY-REGION-CITY.BIN
+$(SERVICES_ROOT)/tmp/ip2l/IP-COUNTRY-REGION-CITY.BIN:
+	mkdir -p $(SERVICES_ROOT)/tmp/ip2l
+	wget -c http://www.clickyab.com/downloads/IP-COUNTRY-REGION-CITY.BIN -O $(SERVICES_ROOT)/tmp/ip2l/IP-COUNTRY-REGION-CITY.BIN
 
 service_bindata:
-	GOPATH=$(SERVICE_ROOT)/tmp $(GO) get -v $(UPDATE) github.com/jteeuwen/go-bindata/go-bindata
+	GOPATH=$(SERVICES_ROOT)/tmp $(GO) get -v $(UPDATE) github.com/jteeuwen/go-bindata/go-bindata
 
-$(SERVICE_ROOT)/ip2location/data.gen.go: $(SERVICE_ROOT)/tmp/ip2l/IP-COUNTRY-REGION-CITY.BIN service_bindata
-	cd $(SERVICE_ROOT)/tmp/ip2l && $(SERVICE_ROOT)/tmp/bin/go-bindata -nomemcopy -o $(SERVICE_ROOT)/ip2location/data.gen.go -pkg ip2location .
+$(SERVICES_ROOT)/ip2location/data.gen.go: $(SERVICES_ROOT)/tmp/ip2l/IP-COUNTRY-REGION-CITY.BIN service_bindata
+	cd $(SERVICES_ROOT)/tmp/ip2l && $(SERVICES_ROOT)/tmp/bin/go-bindata -nomemcopy -o $(SERVICES_ROOT)/ip2location/data.gen.go -pkg ip2location .
+
+services_ip2l: $(SERVICES_ROOT)/ip2location/data.gen.go
 
 services_convey:
 	$(GO) get -v github.com/smartystreets/goconvey/...
 
 services_linter:
 	$(GO) get -v gopkg.in/alecthomas/gometalinter.v1
-	$(LINTER) --install
+	$(SERVICES_LINTER) --install
 
-services_test: $(SERVICE_ROOT)/ip2location/data.gen.go services_convey services_linter
+services_test: services_ip2l services_convey services_linter
 	rm -rf $(GOPATH)/src/services
-	mkdir -p $(GOPATH)/src/services && cp -r $(SERVICE_ROOT)/* $(GOPATH)/src/services/
-	$(LINTERCMD) $(GOPATH)/src/services/...
+	mkdir -p $(GOPATH)/src/services && cp -r $(SERVICES_ROOT)/* $(GOPATH)/src/services/
+	$(SERVICES_LINTERCMD) $(GOPATH)/src/services/...
 	$(GO) get -v github.com/smartystreets/goconvey/...
 	cd $(GOPATH)/src/services && $(GO) get -v ./...
 	cd $(GOPATH)/src/services && $(GO) test -v ./...
