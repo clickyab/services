6981da801d009a2c33d5e8392f779fb3304c7ad4
slifer <dara51php@gmail.com>
2017-09-06 16:26:25 +0430

    fix redlock
    Fix #3496

--- kv/redis/redis-redlock.go
+++ kv/redis/redis-redlock.go
@@ -1,16 +1,13 @@
 package redis
 
 import (
-	"sync"
 	"time"
 
-	"sync/atomic"
-
 	"github.com/clickyab/services/aredis"
-	"github.com/clickyab/services/assert"
 	"github.com/clickyab/services/config"
 	"github.com/clickyab/services/kv"
 	"github.com/clickyab/services/random"
+	"github.com/sirupsen/logrus"
 )
 
 const unlockScript = `
@@ -22,16 +19,12 @@ const unlockScript = `
         `
 
 type mux struct {
-	locker sync.Mutex
-
 	ttl      time.Duration
 	now      time.Time
 	resource string
 
 	value   string
 	retries int
-
-	swap int32
 }
 
 var tryCoolDown = config.RegisterDuration("services.kv.redlock.cooldown", time.Millisecond*200, "cooldown for redlock algorithm in redis")
@@ -40,13 +33,6 @@ var tryCoolDown = config.RegisterDuration("services.kv.redlock.cooldown", time.M
 
 // Lock is used to set a record in redis and tries until it gets its goal
 func (m *mux) Lock() {
-	// Make sure this is locked here, and concurrent call to this
-	// is blocked by the simple mutex.
-	// this is a distributed lock, and also it means it must block the same
-	// call in the same routine too!
-	m.locker.Lock()
-
-	assert.True(atomic.CompareAndSwapInt32(&m.swap, 0, 1), "invalid value on lock")
 	m.now = time.Now()
 
 	m.value = <-random.ID
@@ -64,30 +50,10 @@ func (m *mux) Lock() {
 // Unlock tries to get the record from redis and tries until it can
 func (m *mux) Unlock() {
 	// must be unlocked after the call, this block concurrent call in same process
-	defer m.locker.Unlock()
-
-	assert.True(atomic.LoadInt32(&m.swap) == 1, "not locked?")
-	defer func() {
-		assert.True(atomic.CompareAndSwapInt32(&m.swap, 1, 0), "not locked?")
-	}()
-
 	h := unlockScript
-	for i := 0; i < m.retries; i++ {
-		if time.Since(m.now) > m.TTL() {
-			return
-		}
-
-		cmd := aredis.Client.Eval(h, []string{m.value})
-		if cmd.Err() == nil {
-			if val, ok := cmd.Val().(string); val != m.value || ok == false {
-				time.Sleep(tryCoolDown.Duration())
-				continue
-			}
-		} else {
-			time.Sleep(tryCoolDown.Duration())
-			continue
-		}
-		break
+	cmd := aredis.Client.Eval(h, []string{m.resource}, m.value)
+	if cmd.Err() != nil {
+		logrus.Warn("unlock failed with error :", cmd.Err())
 	}
 }
 
@@ -104,7 +70,7 @@ func (m *mux) TTL() time.Duration {
 // newRedisDistributedLock returns interface of a redlock
 func newRedisDistributedLock(resource string, ttl time.Duration) kv.DistributedLock {
 	return &mux{
-		retries:  int((ttl / tryCoolDown.Duration()).Nanoseconds() / 2),
+		retries:  int((ttl / tryCoolDown.Duration()) + 1),
 		resource: resource,
 		ttl:      ttl,
 	}
