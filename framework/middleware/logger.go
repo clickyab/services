ecbeebd0c8304e7f10bcb96fec0d44b9891a88bc
Kian Ostad <c@kianostad.com>
2017-09-13 12:44:18 +0430

    Add cf-ray to logger

--- framework/middleware/logger.go
+++ framework/middleware/logger.go
@@ -34,6 +34,7 @@ func (w *wrapper) WriteHeader(c int) {
 // Logger is the middleware for log system
 func Logger(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		// Start timer
 		start := time.Now()
 		// Process request
@@ -50,6 +51,7 @@ func Logger(next http.HandlerFunc) http.HandlerFunc {
 				"ClientIP": framework.RealIP(r),
 				"Status":   wr.status,
 				"Len":      wr.total,
+				"Cf-ray":   r.Header.Get("CF-RAY"),
 			},
 		).Debug(http.StatusText(wr.status))
 	}
