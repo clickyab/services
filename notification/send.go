7419209ded2575dd8d9d85d30a0f380d04a19681
fzerorubigd <fzerorubigd@gmail.com>
2017-10-05 14:34:08 +0330

    simplify the mail api
    use the gpkg.in since the github version of gomail is subject to
    break changes

--- notification/send.go
+++ notification/send.go
@@ -1,90 +1,44 @@
 package notification
 
 import (
-	"github.com/clickyab/services/assert"
+	"fmt"
+
 	"github.com/clickyab/services/notification/internal/mail"
 	"github.com/sirupsen/logrus"
 )
 
 type (
-	// Notiftype is the type of a notification
-	Notiftype int
+	// NotifyType is the type of a notification
+	NotifyType int
 )
 
 const (
-	// SMSType is the sms notification platform
-	SMSType Notiftype = iota
+
 	// MailType is the sms notification platform
-	MailType
+	MailType NotifyType = iota
+	// SMSType is the sms notification platform
+	SMSType
 )
 
-type mailConfig struct {
-	Cc       []string
-	Bcc      []string
-	FileAddr string
-}
-
-// SMSConfig is the config for sms notification
-type smsConfig struct{}
-
-// Packet is all client should fill for a notification
-type Packet struct {
-	To       []string
-	Platform Notiftype
-
-	mailConfig *mailConfig
-	smsConfig  *smsConfig
-}
-
-// MaxLength is used to check weather its out of range or not
-func (p Packet) MaxLength() int {
-	if p.Platform == SMSType {
-		return 160
-	}
-
-	// almost unlimited chars for email
-	return 1000000
-}
-
-// SetMailConfig set mail config
-func (p *Packet) SetMailConfig(cc, bcc []string, fileAddr string) {
-	if p.Platform != MailType {
-		logrus.Panic("cant set mail config for a non mail packet")
-	}
-	p.mailConfig = &mailConfig{
-		FileAddr: fileAddr,
-		Bcc:      bcc,
-		Cc:       cc,
-	}
-}
-
-// SetSMSConfig sets sms config
-func (p *Packet) SetSMSConfig(cc, bcc []string, fileAddr string) {
-	if p.Platform != SMSType {
-		logrus.Panic("cant set mail config for a non mail packet")
-	}
-	// TODO needs implementation
-	p.smsConfig = &smsConfig{}
+// Duet is the name contact form for better result in some platform
+// with name support, like email
+type Duet struct {
+	Name    string
+	Contact string
 }
 
 // Send sends a notification by its notification type
-func Send(subject string, msg string, p ...Packet) {
-	for i := range p {
-		assert.True(p[i].MaxLength() > len(msg))
+func Send(platform NotifyType, subject string, msg string, From, To Duet) {
+	switch platform {
+	case MailType:
+		mail.Send(
+			subject,
+			msg,
+			mail.NewEmailNameAddress(From.Contact, From.Name),
+			mail.NewEmailNameAddress(To.Contact, To.Name),
+		)
 
-		switch p[i].Platform {
-		case MailType:
-			conf := p[i].mailConfig
-			if conf == nil {
-				mail.Send(subject, msg, "", []string{}, []string{}, p[i].To...)
-			} else {
-				mail.Send(subject, msg, conf.FileAddr, conf.Cc, conf.Bcc, p[i].To...)
-			}
-		case SMSType:
-			if p[i].smsConfig == nil {
-				logrus.Panic("packet's config must be set")
-			}
-			//do some
-		}
+	default:
+		logrus.WithError(fmt.Errorf("not supported type %d", platform)).Panic("invalid notification type")
 	}
 }
