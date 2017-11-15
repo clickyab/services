2b6e2a74ee9fbd09cd665e2dad4bcc5dbc86f80a
Kian Ostad <c@kianostad.com>
2017-11-15 15:13:01 +0330

    Fix kv atomic

--- kv/redis/redis-aeav_test.go
+++ kv/redis/redis-aeav_test.go
@@ -17,22 +17,21 @@ func TestAeavSpec(t *testing.T) {
 	defer initializer.Initialize()()
 
 	Convey("Test keyvalue store for redis", t, func() {
-		store := newRedisAEAVStore("atest_key")
+		store := newRedisAEAVStore("atest_key", time.Second*100)
 		So(store.Key(), ShouldEqual, "atest_key")
 		Convey("check set and get", func() {
 			store.IncSubKey("test", 1)
 			So(store.SubKey("test"), ShouldEqual, 1)
 			store.IncSubKey("another", 2)
 			So(store.SubKey("another"), ShouldEqual, 2)
-			So(store.Save(time.Hour), ShouldBeNil)
 			So(store.AllKeys(), ShouldResemble, map[string]int64{"test": 1, "another": 2})
 		})
 
-		another := newRedisAEAVStore("atest_key")
+		another := newRedisAEAVStore("atest_key", time.Second*100)
 		So(another.SubKey("another"), ShouldEqual, 2)
 		So(another.AllKeys(), ShouldResemble, map[string]int64{"test": 1, "another": 2})
 
-		yet := newRedisAEAVStore("anot_exist")
+		yet := newRedisAEAVStore("anot_exist", time.Second*100)
 		So(yet.SubKey("anot_exist"), ShouldEqual, 0)
 
 	})
