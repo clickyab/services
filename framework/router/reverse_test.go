d33cdef5d0e7ff251b1fd6a89be733c3330340b8
fzerorubigd <fzerorubigd@gmail.com>
2017-11-19 23:21:02 +0330

    add support for mount point

--- framework/router/reverse_test.go
+++ framework/router/reverse_test.go
@@ -16,18 +16,18 @@ func TestReverse(t *testing.T) {
 
 		d, err := Path("static", map[string]string{"ignore": "me"})
 		So(err, ShouldBeNil)
-		So(d, ShouldEqual, "/test/static/param")
+		So(d, ShouldEqual, mountPoint.String()+"/test/static/param")
 
 		d, err = Path("param", map[string]string{"static": "hi", "param": "bye"})
 		So(err, ShouldBeNil)
-		So(d, ShouldEqual, "/test/hi/bye")
+		So(d, ShouldEqual, mountPoint.String()+"/test/hi/bye")
 
 		d, err = Path("param", map[string]string{"static": "hi"})
 		So(err, ShouldNotBeNil)
 
 		d, err = Path("catch", map[string]string{"static": "hi", "param": "bye"}, "aaa", "bbb")
 		So(err, ShouldBeNil)
-		So(d, ShouldEqual, "/test/hi/bye/aaa/bbb")
+		So(d, ShouldEqual, mountPoint.String()+"/test/hi/bye/aaa/bbb")
 
 	})
 }
