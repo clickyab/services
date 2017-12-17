bacc831dedadc3fb40430ddaecb6e709f48ec512
fzerorubigd <fzerorubigd@gmail.com>
2017-12-17 09:26:03 +0330

    add support for loading private/public pair from file

--- ip2location/client/config.go
+++ ip2location/client/config.go
@@ -13,12 +13,12 @@ type cfgInitializer struct {
 
 func (ci *cfgInitializer) Initialize() config.DescriptiveLayer {
 	l := config.NewDescriptiveLayer()
-	l.Add("DESCRIPTION", "service.ip2location.client.endpoint", "127.0.0.1:8190")
+	l.Add("IP to location service port", "services.ip2location.client.endpoint", "127.0.0.1:8190")
 	return l
 }
 
 func (ci *cfgInitializer) Loaded() {
-	ip2lserver = config.GetStringDefault("service.ip2location.client.endpoint", "127.0.0.1:8190")
+	ip2lserver = config.GetStringDefault("services.ip2location.client.endpoint", "127.0.0.1:8190")
 }
 
 func init() {
