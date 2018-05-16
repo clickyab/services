ca419024023060a11e625f2a7accd15da68b4836
mohsen <mohsenmirzakhany@gmail.com>
2018-05-16 09:34:25 +0430

    arrayToString added to array

--- array/string.go
+++ array/string.go
@@ -1,5 +1,10 @@
 package array
 
+import (
+	"fmt"
+	"strings"
+)
+
 // StringInArray return true if array contain the string
 func StringInArray(in string, arr ...string) bool {
 	for i := range arr {
@@ -9,4 +14,10 @@ func StringInArray(in string, arr ...string) bool {
 	}
 
 	return false
+}
+
+// ArrayToString convert array of int to string with a separator
+func ArrayToString(a []int64, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+
 }
