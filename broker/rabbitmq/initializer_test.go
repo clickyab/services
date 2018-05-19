77782060c5270be6b38f6be5dc7e690cd1f8ae06
Mehrdad Dadkhah <mehrdad@dadkhah.me>
2018-05-19 10:43:21 +0430

    fix rabbit connection ring and add fake rabbit server add test for initialize rabbit connections# Please enter the commit message for your changes. Lines starting refactor rabbit package structure# with '#' will be ignored, and an empty message aborts the commit. fixes #5307

---
+++ broker/rabbitmq/initializer_test.go
@@ -0,0 +1,58 @@
+package rabbitmq
+
+import (
+	"context"
+	"testing"
+
+	"gopkg.in/fzerorubigd/onion.v3"
+
+	"github.com/clickyab/services/broker/rabbitmq/mqfake"
+	"github.com/clickyab/services/config"
+	"github.com/clickyab/services/initializer"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestInitialize(t *testing.T) {
+	config.Initialize("test", "test", "test")
+	defer initializer.Initialize()()
+
+	Convey("Initialize should init some connections and some publishers", t, func() {
+		ctx, cnl := context.WithCancel(context.Background())
+
+		o := onion.New()
+		connection = o.RegisterInt("test.connections.count", 5)
+		publisher = o.RegisterInt("test.publishers.count", 30)
+
+		rb := initRabbit{
+			amqp: &Amqp{
+				Channel:    &mqfake.FakeChannel{},
+				Connection: &mqfake.FakeConnection{},
+				Dial:       &mqfake.FakeDial{},
+			},
+		}
+		rb.Initialize(ctx)
+
+		So(
+			rb.Statistics(),
+			ShouldResemble,
+			map[string]interface{}{
+				"connections":                5,
+				"publishers":                 30,
+				"publishers_per_connections": []int64{6, 6, 6, 6, 6},
+				"jobs": map[string]map[string]int64{
+					"Consume": map[string]int64{
+						"failed":  0,
+						"success": 0,
+					},
+					"Publish": map[string]int64{
+						"failed":  0,
+						"success": 0,
+					},
+				},
+			},
+		)
+
+		defer cnl()
+	})
+}
